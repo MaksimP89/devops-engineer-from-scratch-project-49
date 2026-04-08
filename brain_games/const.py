d171d96a1bfd979321eb1e5ee823cfa8a5b00b39
MaximP <maximponomarenko89@gmail.com>
2026-04-08 15:12:52 +0500

    add Hello

---
+++ brain_games/const.py
@@ -0,0 +1,1 @@
+HELLO = "brain-games\nWelcome to the Brain Games!"
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,12 @@
+import prompt
+
+from brain_games.const import HELLO
+
+
 def main():
-    print("hellow brain")
+    print(HELLO)
+    name = prompt.string("May I have your name?")
+    print(f"Hellow, {name}")
 
 
 if __name__ == "__main__":
