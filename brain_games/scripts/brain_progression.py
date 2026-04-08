88e5a87cc3498d5b2be188620e8d9ccf10c3f3ed
MaximP <maximponomarenko89@gmail.com>
2026-04-08 22:22:55 +0500

    br progression done

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -5,7 +5,6 @@ from brain_games.cli import welcome_user
 from brain_games.const import (
     ANSWER_OUT,
     CONGL,
-    CORRECT,
     EXIT_2,
     GAME,
     INPUT_FROM_USER,
@@ -15,32 +14,41 @@ from brain_games.utils import (
     exit_from_game,
     get_input_from_user,
     get_int_from_user,
+    main_execution,
 )
 
 
 def question():
-    
-    return expr, result
+    length = 10
+    start = randint(1, 11)
+    step = randint(1, 5)
+    progression = [start + i * step for i in range(length)]
+    hidden_index = randint(0, length - 1)
+    correct_answer = progression[hidden_index] 
+    progression[hidden_index] = '..'
+    print(correct_answer)
+    expr = ' '.join(str(x) for x in progression)
+    return expr, str(correct_answer)
 
 
 def main():
     USER_NAME = welcome_user()
     run_game = 0
-    print(QUESTION_3)
+    print(QUESTION_4)    
     while run_game <= GAME:
-        expr, result = question()        
-        inp = get_input_from_user(INPUT_FROM_USER.format(expr))
-        inp = get_int_from_user(inp)
-        print(ANSWER_OUT.format(inp))
-
-        if inp == result:
-            print(CORRECT)
-            run_game += 1
-        else:
-            exit_from_game(EXIT_2.format(inp, result, USER_NAME))
-
+        expr, r_result = question()
+        imput_value = get_input_from_user(INPUT_FROM_USER.format(expr))       
+        run_game += main_execution(
+            answer_out=ANSWER_OUT.format(imput_value),
+            inp_user=imput_value,
+            expect=r_result,
+            exite_string=EXIT_2,
+            format_ex_str=[imput_value,r_result,USER_NAME],            
+        )
+        
     print(CONGL.format(USER_NAME))
 
+    
 
 if __name__ == "__main__":
     main()
