94d21f22d87220ac8950363cd9b367acd6015bde
MaximP <maximponomarenko89@gmail.com>
2026-04-11 00:12:26 +0500

    fix comst

--- brain_games/logic/const.py
+++ brain_games/logic/const.py
@@ -1,18 +1,18 @@
-HELLO = "brain-games\nWelcome to the Brain Games!"
-QUESTION_1 = "Answer 'yes' if the number is even, otherwise answer 'no'."
-QUESTION_2 = "What is the result of the expression?"
-QUESTION_3 = "Find the greatest common divisor of given numbers."
-QUESTION_4 = "What number is missing in the progression?"
-QUESTION_5 = 'Answer "yes" if given number is prime. Otherwise answer "no"'
+HELLO = "Welcome to the Brain Games!"
+QUESTION_1 = 'Answer "yes" if the number is even, otherwise answer "no".'
+QUESTION_2 = 'What is the result of the expression?'
+QUESTION_3 = 'Find the greatest common divisor of given numbers.'
+QUESTION_4 = 'What number is missing in the progression?'
+
 EXIT = ("'{}' is wrong answer ;(. "
     "Correct answer was '{}'.\nLet's try again, {}!")
-CONGL = "Congratulations, {}!"
-INPUT_FROM_USER = "Question: {}"
-EXCEPT_CHISLO = "Введите число."
+CONGL = 'Congratulations, {}!'
+INPUT_FROM_USER = 'Question: {}'
+EXCEPT_CHISLO = 'Введите число.'
 GAME = 3
-PROGRAM_NACHALO = "May I have your name? "
-ANSWER_OUT = "Your answer: {}"
-CORRECT = "Correct!"
+PROGRAM_NACHALO = 'May I have your name?'
+ANSWER_OUT = 'Your answer: {}'
+CORRECT = 'Correct!'
 GAMES = ("Выберете игру в которую вам хотелось бы сыграть.\nВведите цифру:" 
 "\n1-Проверка на чётность\n2-Калькулятор\n3-НОД\n4-Арифметическая прогрессия"
 "\n5-Простое ли число")
