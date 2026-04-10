6a2eebe4a8623fba779d3a636aa72f2cf87f7b30
MaximP <maximponomarenko89@gmail.com>
2026-04-10 22:39:08 +0500

    fix brain games

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,11 @@
-from brain_games.logic.utils import welcome_user
+from brain_games.cli import welcome_user
+from brain_games.logic.utils import get_input_from_user
 
 
 def main():
-    welcome_user()
+    USER_NAME = get_input_from_user()
+    USER_NAME = USER_NAME.capitalize()
+    print(f"Hellow, {USER_NAME}")
 
 
 if __name__ == "__main__":
